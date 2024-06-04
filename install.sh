032f4ff86a41a83274955d742c3451830d75403d
Brady MacDonald <brady.macdonald@dal.ca>
2024-06-04 11:27:05 -0400

    install updates

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install i3 feh picom polybar rofi dunst
+sudo pacman -S alacritty feh picom polybar rofi dunst zsh
 
-ln -s ~/dotfiles/.gitconfig ~
-ln -s ~/dotfiles/alacritty ~/.config
-ln -s ~/dotfiles/dunst ~/.config
-ln -s ~/dotfiles/picom ~/.config
-ln -s ~/dotfiles/polybar ~/.config
-ln -s ~/dotfiles/rofi ~/.config
-ln -s ~/dotfiles/tmux ~/.config
-ln -s ~/dotfiles/zsh ~/.config
+ln -sf ~/dotfiles/.gitconfig ~
+ln -sf ~/dotfiles/wallpaper.jpg ~/.config
+ln -sf ~/dotfiles/alacritty ~/.config
+ln -sf ~/dotfiles/dunst ~/.config
+ln -sf ~/dotfiles/i3 ~/.config
+ln -sf ~/dotfiles/picom ~/.config
+ln -sf ~/dotfiles/polybar ~/.config
+ln -sf ~/dotfiles/rofi ~/.config
+ln -sf ~/dotfiles/tmux ~/.config
+ln -sf ~/dotfiles/zsh ~/.config
