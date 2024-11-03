0dba2eacc42251f710b0f7baa13d2dac55b7e70f
Brady MacDonald <brady.macdonald@dal.ca>
2024-11-03 16:26:53 -0500

    hyprecosystem updates

--- install.sh
+++ install.sh
@@ -15,13 +15,13 @@ sudo pacman -Syu alacritty rofi-wayland tmux zsh
 # AUR
 # vlc-git signal
 
-ln -sf ~/dotfiles/.gitconfig ~
-ln -sf ~/dotfiles/scripts ~/.config
-ln -sf ~/dotfiles/wallpapers ~/.config
-ln -sf ~/dotfiles/alacritty ~/.config
-ln -sf ~/dotfiles/waybar ~/.config
-ln -sf ~/dotfiles/dunst ~/.config
-ln -sf ~/dotfiles/rofi ~/.config
-ln -sf ~/dotfiles/tmux ~/.config
-ln -sf ~/dotfiles/hypr ~/.config
-ln -sf ~/dotfiles/zsh ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/.gitconfig ~
+ln -sf ~/Brady-MacDonald/dotfiles/scripts ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/wallpapers ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/alacritty ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/waybar ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/dunst ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/rofi ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/tmux ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/hypr ~/.config
+ln -sf ~/Brady-MacDonald/dotfiles/zsh ~/.config
