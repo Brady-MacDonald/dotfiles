8050aac926339f1de75693ba4d4560179acd7d3f
Brady MacDonald <brady.macdonald@dal.ca>
2024-12-11 11:28:03 -0400

    Work on install script

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 #===============================================================================
 #
-#                NOTES: Brady-MacDonald Install Script
-#                repo to your home folder as ~/dotfiles/
+#                    Brady-MacDonald Install Script
+#                clone to your home folder as ~/dotfiles/
 #
 #==============================================================================
 
 packages=""
 
+pwd=$(pwd)
 user=$(whoami)
-echo -e "Hello, ${user}\n"
+echo -e "Hello ${user}\n"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -25,41 +26,53 @@ function prompt {
         return 1
     fi
 }
-prompt "Install Hyprland?"
+
+# Install required packages
+prompt "Install wayland/Hyprland?"
 val=$?
 echo -en "-> ${ORANGE}Hyprland${NC}... "
 if [[ $val -eq 0 ]]; then
     echo -e "${GREEN}lets get it${NC}"
-    packages+="hyprland hyprpaper hyprlock hypridle "
+    packages+="wayland hyprland hyprpaper hyprlock hypridle "
 else
     echo -e "${RED}skipped${NC}"
 fi
 
-prompt "Install other dependecies?"
+prompt "Install other required dependecies? (zsh/rofi/waybar ...)"
 val=$?
-echo -en "-> ${ORANGE}Others${NC}... "
+echo -en "-> ${ORANGE}Required packages${NC}... "
 if [[ $val -eq 0 ]]; then
-    packages+="alacritty rofi-wayland tmux zsh"
+    packages+="waybar alacritty rofi-wayland tmux swaync zsh"
     echo -e "${GREEN}lets get it${NC}"
 else
     echo -e "${RED}skipped${NC}"
 fi
 
-echo $packages
+prompt "Install helpful stuff? (bluetooth/brightness/yazi...)"
+val=$?
+echo -en "-> ${ORANGE}Others${NC}... "
+if [[ $val -eq 0 ]]; then
+    packages+="bluez bluez-utils yazi playerctl brightnessctl"
+    echo -e "${GREEN}lets get it${NC}"
+else
+    echo -e "${RED}skipped${NC}"
+fi
 
+echo \n $packages \n
 
-prompt "Create symbolic links with $XDG_CONFIG_HOME?"
-if [[ links == "y"]]; then
-    ln -sf ~/Brady-MacDonald/dotfiles/.gitconfig ~
-    ln -sf ~/Brady-MacDonald/dotfiles/scripts $XDG_CONFIG_HOME
-    ln -sf ~/Brady-MacDonald/dotfiles/wallpapers $XDG_CONFIG_HOME
-    ln -sf ~/Brady-MacDonald/dotfiles/alacritty ~/.config
-    ln -sf ~/Brady-MacDonald/dotfiles/waybar ~/.config
-    ln -sf ~/Brady-MacDonald/dotfiles/dunst ~/.config
-    ln -sf ~/Brady-MacDonald/dotfiles/rofi ~/.config
-    ln -sf ~/Brady-MacDonald/dotfiles/tmux ~/.config
-    ln -sf ~/Brady-MacDonald/dotfiles/hypr ~/.config
-    ln -sf ~/Brady-MacDonald/dotfiles/zsh ~/.config
+prompt "Create symbolic links with \$XDG_CONFIG_HOME -> $XDG_CONFIG_HOME?"
+val=$?
+if [[ $val -eq 0 ]]; then
+
+    ln -sfv $pwd/.gitconfig ~
+    ln -sfv $pwd/zsh $XDG_CONFIG_HOME
+    ln -sfv $pwd/hypr $XDG_CONFIG_HOME
+    ln -sfv $pwd/rofi $XDG_CONFIG_HOME
+    ln -sfv $pwd/tmux $XDG_CONFIG_HOME
+    ln -sfv $pwd/waybar $XDG_CONFIG_HOME
+    ln -sfv $pwd/scripts $XDG_CONFIG_HOME
+    ln -sfv $pwd/alacritty $XDG_CONFIG_HOME
+    ln -sfv $pwd/wallpapers $XDG_CONFIG_HOME
 fi
 
 read -p "Install yay? (Y/n)" links
